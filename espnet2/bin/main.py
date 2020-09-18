8a7ce63c889ac076c5075416cf31036707e88ef7
Tilo Himmelsbach <dertilo@gmail.com>
2020-09-18 19:17:06 +0200

    run_asr_task

--- espnet2/bin/main.py
+++ espnet2/bin/main.py
@@ -43,8 +43,8 @@ def write_vocabulary(tokenizer_dir="data/token_list/bpe_unigram5000"):
     tokenize(**kwargs)
 
 
-def train_tokenizer(vocab_size=5000, tokenizer_dir="data/token_list/bpe_unigram"):
-    tokenizer_dir=f"{tokenizer_dir}{vocab_size}"
+def train_tokenizer(vocab_size=5000, tokenizer_dir="data/token_list/bpe_unigram5000"):
+
     os.makedirs(tokenizer_dir, exist_ok=True)
     spm_args = dict(
         input=(f"{tokenizer_dir}/train.txt"),
@@ -65,44 +65,59 @@ def train_tokenizer(vocab_size=5000, tokenizer_dir="data/token_list/bpe_unigram"
     write_vocabulary(tokenizer_dir)
 
 
-def dryrun_to_calc_stats(
-    train_set="debug_train",
-    valid_set="debug_dev",
-    test_sets="dev_clean",
-    nbpe=5000,
-    asr_config="conf/tuning/train_asr_transformer_tiny.yaml",
-    inference_config="conf/decode_asr.yaml",
+def run_asr_task(
+    output_dir, num_gpus=0, is_distributed=False, num_workers=0, collect_stats=False
 ):
 
-    cmd = f"""
-    echo $(which python) &&
-    ./asr.sh \
-    --lang en \
-    --ngpu 0 \
-    --use_lm false \
-    --stage 9 \
-    --stop_stage 9 \
-    --nbpe {nbpe} \
-    --max_wav_duration 30 \
-    --asr_config {asr_config} \
-    --inference_config {inference_config} \
-    --train_set {train_set} \
-    --valid_set {valid_set} \
-    --test_sets {test_sets} \
-    --srctexts "data/{train_set}/text" "$@"
-    """
-
-    print(util_methods.exec_command(cmd))
+    argString = (
+        f"--collect_stats {collect_stats} "
+        f"--use_preprocessor true "
+        f"--bpemodel {tokenizer_path}/bpe.model "
+        f"--seed 42 "
+        f"--num_workers {num_workers} "
+        f"--token_type bpe "
+        f"--token_list {tokenizer_path}/tokens.txt "
+        f"--g2p none "
+        f"--non_linguistic_symbols none "
+        f"--cleaner none "
+        f"--resume true "
+        f"--fold_length 5000 "
+        f"--fold_length 150 "
+        f"--config {config} "
+        f"--frontend_conf fs=16k "
+        f"--output_dir {output_dir} "
+        f"--train_data_path_and_name_and_type {train_manifest_path}/wav.scp,speech,sound "
+        f"--train_data_path_and_name_and_type {train_manifest_path}/text,text,text "
+        f"--valid_data_path_and_name_and_type {dev_manifest_path}/wav.scp,speech,sound "
+        f"--valid_data_path_and_name_and_type {dev_manifest_path}/text,text,text "
+        f"--ngpu {num_gpus} "
+        f"--multiprocessing_distributed {is_distributed} "
+    )
+    if not collect_stats:
+        argString += (
+            f"--train_shape_file {stats_dir}/train/speech_shape "
+            f"--train_shape_file {stats_dir}/train/text_shape "
+            f"--valid_shape_file {stats_dir}/valid/speech_shape "
+            f"--valid_shape_file {stats_dir}/valid/text_shape "
+        )
+    else:
+        argString += (
+            f"--train_shape_file {train_manifest_path}/wav.scp "
+            f"--valid_shape_file {dev_manifest_path}/wav.scp "
+        )
+    parser = ASRTask.get_parser()
+    args = parser.parse_args(shlex.split(argString))
+    ASRTask.main(args=args)
 
 
 if __name__ == "__main__":
     import shlex
 
     num_workers = 0
-    tokenizer_path = "data/token_list/bpe_unigram5000"
-    bpe_model = f"{tokenizer_path}/bpe.model"
+    vocab_size = 5000
+    tokenizer_path = f"data/token_list/bpe_unigram{vocab_size}"
+
     stats_dir = "exp/asr_stats_raw"
-    output_dir = "exp/asr_train_asr_transformer_tiny_raw_bpe"
     config = "conf/tuning/train_asr_transformer_tiny.yaml"
 
     # manifest_path = "/tmp/espnet_manifests"
@@ -113,42 +128,24 @@ if __name__ == "__main__":
 
     train_manifest_path = f"{manifest_path}/{train_name}"
     dev_manifest_path = f"{manifest_path}/{valid_name}"
+
     build_manifest_files(train_manifest_path)
     build_manifest_files(dev_manifest_path)
 
-    vocab_size = 5000
-    tokenizer_dir="data/token_list/bpe_unigram"
-    if not os.path.isdir(tokenizer_dir):
-        train_tokenizer(vocab_size,tokenizer_dir)
+    if not os.path.isdir(tokenizer_path):
+        train_tokenizer(vocab_size, tokenizer_path)
 
     if not os.path.isdir(stats_dir):
-        dryrun_to_calc_stats(train_set=train_name, valid_set=valid_name)
+        run_asr_task(collect_stats=True, output_dir=f"{stats_dir}")
+
+    assert os.path.isdir(stats_dir)
 
     num_gpus = 0
     is_distributed = False
-    argString = (
-        f"--use_preprocessor true --bpemodel {bpe_model} --seed 42 --num_workers {num_workers} --token_type bpe "
-        f"--token_list {tokenizer_path}/tokens.txt "
-        f"--g2p none --non_linguistic_symbols none --cleaner none "
-        f"--valid_data_path_and_name_and_type {dev_manifest_path}/wav.scp,speech,sound "
-        f"--valid_data_path_and_name_and_type {dev_manifest_path}/text,text,text "
-        f"--resume true --fold_length 5000 --fold_length 150 "
-        f"--output_dir {output_dir} --config {config} --frontend_conf fs=16k "
-        f"--train_data_path_and_name_and_type {train_manifest_path}/wav.scp,speech,sound "
-        f"--train_data_path_and_name_and_type {train_manifest_path}/text,text,text "
-
-        f"--train_shape_file {stats_dir}/train/speech_shape "
-        f"--train_shape_file {stats_dir}/train/text_shape.bpe "
-        f"--valid_shape_file {stats_dir}/valid/speech_shape "
-        f"--valid_shape_file {stats_dir}/valid/text_shape.bpe "
-
-        f"--ngpu {num_gpus} "
-        f"--multiprocessing_distributed {is_distributed}"
+    run_asr_task(
+        num_workers=0,
+        output_dir=f"exp/{config.split('/')[-1].replace('.yaml','')}",
     )
-
-    parser = ASRTask.get_parser()
-    args = parser.parse_args(shlex.split(argString))
-    ASRTask.main(args=args)
 
     """
     LRU_CACHE_CAPACITY=1 python ~/code/SPEECH/espnet/espnet2/bin/main.py
@@ -156,4 +153,6 @@ if __name__ == "__main__":
     [tilo-ThinkPad-X1-Carbon-6th] 2020-09-17 18:14:26,464 (trainer:243) INFO: 1epoch results: [train] iter_time=0.164, forward_time=0.952, loss=480.386, loss_att=191.654, loss_ctc=1.154e+03, acc=1.022e-04, backward_time=0.973, optim_step_time=0.007, lr_0=2.080e-06, train_time=2.105, time=1 minute and 43.19 seconds, total_count=49, [valid] loss=479.955, loss_att=191.637, loss_ctc=1.153e+03, acc=1.030e-04, cer=1.010, wer=1.000, cer_ctc=4.993, time=50.5 seconds, total_count=49, [att_plot] time=3.2 seconds, total_count=0
     [tilo-ThinkPad-X1-Carbon-6th] 2020-09-17 18:14:28,067 (trainer:286) INFO: The best model has been updated: valid.acc
     [tilo-ThinkPad-X1-Carbon-6th] 2020-09-17 18:14:28,068 (trainer:322) INFO: The training was finished at 1 epochs 
+    
+
     """
